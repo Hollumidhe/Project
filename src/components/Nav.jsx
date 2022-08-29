de8250ee8a811fded3cfaa101b2c635ec5e047f7
Michael Olumide <olubimimichael01@gmail.com>
2022-08-29 18:49:55 +0100

    portfolio commited

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Nav = () => {
+  return (
+    <div>
+        <h1 className=' text-9xl mx-10 mix-blend-color'>Nav</h1>
+    </div>
+  )
+}
+
+export default Nav
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    
+    './src/components/**/*.{html,js,jsx}',
+  ],
+  content: [],
+  theme: {
+    extend: {},
+    fontFamily: {
+      signature: ["Great Vibes"],
+    },
+  },
+  plugins: [],
+};
